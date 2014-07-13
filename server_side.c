636965ceff51c2a6e035dee9626ae66385ae5cf0
Jonathan Acevedo <jonathan@ginger.io>
2014-07-12 23:37:30 -0400

    added open and server close

--- server_side.c
+++ server_side.c
@@ -1,3 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "ece454rpc_types.h"
+
+using namespace std;
+
+typedef struct ServerFile{
+	int fd;
+	bool locked;
+	ServerFile *next;
+} ServerFile;
+
+ServerFile *sf_head;
+ServerFile *latest_file;
+head = (ServerFile *)malloc(sizeof(ServerFile));
+head->fd = NULL;
+head->locked = false;
 
 typedef struct Node {
   char *ip;
@@ -6,38 +25,38 @@ typedef struct Node {
 
 Node *root;
 Node *tail;
-root = (Node *)malloc(sizeof(Node));
+root = malloc(sizeof(Node));
 root->ip = "";
 root->next = NULL;
 tail = root;
 
-return_type add(const int nparams, arg_type* a)
-{
-    if(nparams != 2) {
-	/* Error! */
-	r.return_val = NULL;
-	r.return_size = 0;
-	return r;
-    }
+return_type r;
 
-    if(a->arg_size != sizeof(int) ||
-       a->next->arg_size != sizeof(int)) {
-	/* Error! */
-	r.return_val = NULL;
-	r.return_size = 0;
-	return r;
-    }
+int authenticate(char *user_ip) {
+	Node *curr = root;
 
-    int i = *(int *)(a->arg_val);
-    int j = *(int *)(a->next->arg_val);
+	while (curr != NULL) {
+		if (curr->ip == user_ip) {
+			return 1;
+		}
+		curr = curr->next;
+	}
 
-    int *ret_int = (int *)malloc(sizeof(int));
+	return 0;
+}
 
-    *ret_int = i+j;
-    r.return_val = (void *)(ret_int);
-    r.return_size = sizeof(int);
+int authorize_file(int user_fd) {
+	Node *curr = sf_head;
 
-    return r;
+	while (curr != NULL) {
+		if (curr->fd == user_fd) {
+			if (curr->locked == true) { return 0 }
+			return 1;
+		}
+		curr = curr->next;
+	}
+
+	return -1;
 }
 
 return_type mount(const int nparams, arg_type* a)
@@ -61,8 +80,8 @@ return_type mount(const int nparams, arg_type* a)
 	tail->next = new_node;
 	tail = new_node;
 
-	r.return_size = sizeof(int);
 	r.return_val = 1;
+	r.return_size = sizeof(int);
 
     return r;
 }
@@ -96,15 +115,72 @@ return_type unmount(const int nparams, arg_type* a)
 			return r;
 		}
 
-		prev = head->next;
+		prev = prev->next;
 		curr = curr->next;
 	}
 
     return r;
 }
 
+return_type fsopen(const int nparams, arg_type* a)
+{
+	if (nparams != 3) {
+		r.return_val = NULL;
+		r.return_size = 0;
+		return r;
+	}
 
-// array activeClients = new array();
+	return_type error_val;
+	error_val.return_val = -1;
+	error_val.return_size = sizeof(int);
+
+	char *user_ip = (char *)a->arg_val;
+	if (authenticate(user_ip) == 0) {return error_val;}
+
+	char *filepath = (char *)a->next->arg_val;
+	int mode = (int)a->next->next->arg_val;
+
+	struct stat buffer;
+
+	int err = stat(filepath, &buffer);
+	if (err == -1 && mode == 0) {return error_val;}
+	if (S_ISDIR(buffer.st_mode)) {return error_val;} 
+
+	int fd;
+	if (mode == 1) {
+		fd = open(filepath, O_CREAT | O_WR_ONLY | O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR);
+	}
+	else {
+		fd = open(filepath, O_RDONLY);
+	}
+
+	r.return_val = fd;
+	r.return_size = sizeof(int);
+	
+	return r;
+}
+
+return_type fsclose(int nparams, arg_type* a) {
+	if (nparams != 2) {
+		r.return_val = NULL;
+		r.return_size = 0;
+	}
+
+	int fd = a->arg_val;
+	char *user_ip = (char *)a->next->arg_val;
+
+	if (authenticate(user_ip) == 0) {
+		r.return_val = -1
+		r.return_size = sizeof(int)
+		return r
+;	}
+	
+	r.return_val = close(fd);
+	r.return_size(sizeof(int));
+	return r;
+}
+
+return_type fsread(int nparams, arg_type* a) {}
 
 return_type opendir(foldername) {
 	// Start searching for foldername from root using bredth-first-search
@@ -115,7 +191,3 @@ return_type opendir(foldername) {
 	// else:
 	//		return error_num
 }
-
-int mount(clientaddr, clientport){
-	// activeClients.push(clientaddr + clientport);
-}\
