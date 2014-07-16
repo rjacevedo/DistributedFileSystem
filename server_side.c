255149e3e2a5519f1596f841e3910e0805fff405
Javier Ching <jhnching@uwaterloo.ca>
2014-07-15 22:34:08 -0400

    all functions implemented except for remove

--- server_side.c
+++ server_side.c
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "ece454rpc_types.h"
+#include "stack.c"
 
-
+//should have linked list for mounted and opened files
 typedef struct UsedFile{
 	int fd;
 	bool locked;
@@ -18,20 +19,51 @@ typedef struct MountedUser {
   struct MountedUser *next;
 } MountedUser;
 
-return_type r;
+typedef struct OpenedFile {
+    int fd;
+    char *ip;
+    struct OpenedFile *next;
+} OpenedFile;
+
+typedef struct File {
+    char *filename;
+    unsigned char type;
+    struct File *next;
+} File;
+
+typedef struct FSDIR {
+    File *files;
+    File *currentFile;
+    DIR *parent;
+} FSDIR;
 
 UsedFile *uf_head;
 UsedFile *latest_file;
+OpenedFile *op_head;
+OpenedFile *op_current;
+OpenedFile *op_tail;
 MountedUser *root;
 MountedUser *tail;
 
+return_type r;
+return_type error_val;
+error_val.return_val = (void *) -1;
+error_val.return_size = sizeof(int);
+
 int main(int argc, char*argv[]) {
-	uf_head = malloc(sizeof(UsedFile));
+	uf_head = (UsedFile *)malloc(sizeof(UsedFile));
 	uf_head->fd = 0;
 	uf_head->locked = false;
 	latest_file = uf_head;
 
-	root = malloc(sizeof(MountedUser));
+    op_head = (MountedUser *)malloc(sizeof(OpenedFile));
+    op_head->fd = 0;
+    op_head->ip = NULL;
+    op_head->next = NULL;
+    op_tail = op_head;
+    op_current = op_head;
+
+	root = (MountedUser *)malloc(sizeof(MountedUser));
 	root->ip = "";
 	root->next = NULL;
 	tail = root;
@@ -81,7 +113,7 @@ return_type fsMount(const int nparams, arg_type* a)
 
 	char *user_ip = (char *)a->arg_val;
 
-	MountedUser *new_MountedUser = malloc(sizeof(MountedUser));
+	MountedUser *new_MountedUser = (MountedUser *)malloc(sizeof(MountedUser));
 	new_MountedUser->ip = user_ip;
 	new_MountedUser->next = NULL;
 	tail->next = new_MountedUser;
@@ -130,7 +162,7 @@ return_type fsUnmount(const int nparams, arg_type* a)
     return r;
 }
 
-//nparams: user_ip -> filename
+//nparams: user_ip -> filepath
 return_type fsOpenDir(int nparams, arg_type* a) {
 	// Use stat to check if folder already exists
 	// if exists
@@ -142,61 +174,125 @@ return_type fsOpenDir(int nparams, arg_type* a) {
     if (nparams != 2) {
 		r.return_val = NULL;
 		r.return_size = 0;
+		return r;
 	}
-
-	return_type error_val;
-	error_val.return_val = (void *) -1;
-	error_val.return_size = sizeof(int);
 
 	char *user_ip = (char *)a->arg_val;
 	if (authenticate(user_ip) == 0) {return error_val;}
 
-    char *filename = (char *)a->next->arg_val;
+    char *filepath = (char *)a->next->arg_val;
 
 	DIR *d = NULL;
     struct dirent *dir;
-    char filepath[500];
+    struct stat buffer;
+    int err;
+    File *f_head = NULL;
+    File *f_tail = NULL;
+    File *f_newFile = NULL;
+    FSDIR *fsdir = (FSDIR *)malloc(sizeof(FSDIR));
 
-    d = opendir(".");
+	err = stat(filepath, &buffer);
+	if (err == -1) {
+        r.return_val = (void *)NULL;
+        r.return_size = 0;
+        return r;
+    }
+
+    d = opendir(filepath);
+    //TODO: need to figure out better algorithm to find folder
     if (d){
         while ((dir = readdir(d)) != NULL){
-            //pop onto the stack
-            if(dir->d_name == filename){
+            if(f_head == NULL){
+                f_head = (File *)malloc(sizeof(File));
+                f_head->filename = dir->d_name;
+                f_tail = f_head;
 
+                //TODO: check the filepath
+                err = stat(filepath+dir->d_name, &buffer);
+                if(err == -1){
+                    f_head->type = -1;
+                }
+                if (S_ISDIR(buffer.st_mode)) {
+                    f_head->type = 0
+                }else{
+                    f_head->type = 1;
+                }
+            }else{
+                f_newFile = (File *)malloc(sizeof(File));
+                f_newFile->filename = dir->d_name;
+                f_tail->next = f_newFile;
+                f_tail = f_newFile;
             }
         }
         closedir(d);
     }
 
-	struct stat buffer;
-
-	int err = stat(filepath, &buffer);
-	if (err == -1) {return error_val;}
-	if (S_ISDIR(buffer.st_mode)) {
-
-	}
+    fsdir->files = f_head;
+    fsdir->currentFile = f_head;
+    fsdir->parent = d;
+    r.return_val = (void *)fsdir;
+    r.return_size = sizeof(fsdir);
+    return r;
 }
 
+//params: FSDIR*
 return_type fsCloseDir(int nparams, arg_type* a){
-
-}
-
-return_type fsReadDir(int nparams, arg_type* a){
-
-}
-
-//params: user_ip -> filepath -> mode
-return_type fsOpen(const int nparams, arg_type* a)
-{
-	if (nparams != 3) {
+    if (nparams != 1) {
 		r.return_val = NULL;
 		r.return_size = 0;
 		return r;
 	}
 
-	return_type error_val;
-	error_val.return_val = (void *) -1;
-	error_val.return_size = sizeof(int);
+    FSDIR *fsdir = a->arg_val;
+
+    int err = closedir(d);
+    if(err == -1){
+        return error_val;
+    }
+
+    //free the pointer or its value?
+    free(fsdir);
+
+    r.return_val = (void *)0;
+    r.return_size = sizeof(int);
+    return r;
+}
+
+//nparams: FSDIR*
+return_type fsReadDir(int nparams, arg_type* a){
+    if (nparams != 1) {
+		r.return_val = NULL;
+		r.return_size = 0;
+		return r;
+	}
+
+    FSDIR *fsdir = a.arg_val;
+
+    struct fsDirent *fsdirent = (struct fsDirent *)malloc(sizeof(struct fsDirent));
+
+    if(fsdir->currentFile != NULL){
+        fsdirent->entName = fsdir->currentFile->filename;
+        fsdirent->entType = fsdir->currentFile->type;
+
+        fsdir->currentFile = fsdir->currentFile->next;
+
+        r.return_val = (void *)fsdirent;
+        r.return_size = sizeof(fsdirent);
+        return r;
+    }else{
+        r.return_val = NULL;
+        r.return_size = 0;
+        return r;
+    }
+}
+
+//params: user_ip -> filepath -> mode
+return_type fsOpen(const int nparams, arg_type* a){
+	if (nparams != 3) {
+		r.return_val = NULL;
+		r.return_size = 0;
+		return r;
+	}
 
 	char *user_ip = (char *)a->arg_val;
 	if (authenticate(user_ip) == 0) {return error_val;}
@@ -207,14 +303,18 @@ return_type fsOpen(const int nparams, arg_type* a)
 	struct stat buffer;
 
 	int err = stat(filepath, &buffer);
-	if (err == -1 && mode == 0) {return error_val;}
-	if (S_ISDIR(buffer.st_mode)) {return error_val;}
+	if (err == -1 && mode == 0) {
+        return error_val;
+    }
+	if (S_ISDIR(buffer.st_mode)) {
+        return error_val;
+    }
 
 	int fd;
 	if (mode == 1) {
 		fd = open(filepath, O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR);
         //set lock here so others cannot use the file
-		UsedFile *openedFile;
+		/*UsedFile *openedFile;
 		if(uf_head->fd != 0){
             openedFile->fd = fd;
             openedFile->locked = true;
@@ -223,15 +323,24 @@ return_type fsOpen(const int nparams, arg_type* a)
             uf_head->fd = fd;
             uf_head->locked = true;
             latest_file = uf_head;
-		}
-	}
-	else {
-		fd = open(filepath, O_RDONLY);
+		}*/
+	}else {
+		fd = open(filepath, O_RDONLY | S_IRUSR);
 	}
 
-	r.return_val = fd;
+	if(op_head->fd == 0){
+        op_head->fd = fd;
+        op_head->ip = user_ip;
+	}else{
+        OpenedFile op_newFile = (OpenedFile *)sizeof(OpenedFile);
+        op_newFile->fd = fd;
+        op_newFile->ip = user_ip;
+        op_tail->next = op_newFile;
+        op_tail = op_tail->next;
+	}
+
+	r.return_val = (void *)fd;
 	r.return_size = sizeof(int);
-
 	return r;
 }
 
@@ -240,20 +349,44 @@ return_type fsClose(int nparams, arg_type* a) {
 	if (nparams != 2) {
 		r.return_val = NULL;
 		r.return_size = 0;
+		return r;
 	}
 
 	char *user_ip = (char *)a->arg_val;
     int fd = a->next->arg_val;
 
 	if (authenticate(user_ip) == 0) {
-		r.return_val = -1;
+		r.return_val = (void *)-1;
 		r.return_size = sizeof(int);
 		return r
 	}
 
-	r.return_val = close(fd);
-	r.return_size(sizeof(int));
-	return r;
+    OpenedFile op_prev;
+    op_current = op_head;
+    op_prev = op_current;
+    while(op_current != NULL){
+        if(op_current->fd == fd && op_current->ip == user_ip){
+            if(op_current == op_head){
+                OpenedFile op_newHead = op_head->next;
+                free(op_head);
+                op_head = op_newHead;
+            }else{
+                OpenedFile op_newCurrent = op_current->next;
+                free(op_current);
+                op_prev->next = op_newCurrent;
+                op_current = op_newCurrent;
+            }
+            r.return_val = (void *)close(fd);
+            r.return_size = sizeof(int);
+            return r;
+        }
+        if(op_current != op_head){
+            op_prev = op_prev->next;
+        }
+        op_current = op_current->next;
+    }
+
+    return error_val;
 }
 
 bool isLocked(int fd) {
@@ -265,7 +398,7 @@ bool isLocked(int fd) {
         		return true;
         	}
         	return false;
-            
+
         }
         current = current->next;
     }
@@ -279,6 +412,7 @@ return_type fsRead(int nparams, arg_type* a) {
     if (nparams != 2) {
 		r.return_val = NULL;
 		r.return_size = 0;
+		return r;
 	}
 
 	//TODO: make sure it works concurrently
@@ -286,42 +420,64 @@ return_type fsRead(int nparams, arg_type* a) {
 	// from two different clients?
 	int fd = a->arg_val;
 	int count = a->next->arg_val;
+    char *buffer = malloc(count*sizeof(char));
+    int bytesRead = -1;
 
-    char buffer[count];
-    
-    return_type err;
-    err.return_val = -1;
-    err.return_size = sizeof(int);
+    op_current = op_head;
+    while(op_current != NULL){
+        if(op_current->fd == fd && op_current->fd == fd){
+            bytesRead = read(fd, buffer, count);
 
+            r.return_val = (void *)buffer;
+            r.return_size = sizeof(buffer);
+            return r;
+        }
+        op_current = op_current->next;
+    }
+
+    return error_val;
     // TODO: If a user gets locked, the process should eventually finish the read
     // How do we take care of this? Poll periodically.
 
-    bool locked = isLocked(fd);
+    /*bool locked = isLocked(fd);
     if(!locked){
         if (read(fd, buffer, count) == -1) {
-        	return err;
+        	return error_val;
         }
 
         r.return_val = buffer;
         r.return_size = sizeof(buffer);
     } else{
-        return err;
+        return error_val;
     }
 
-    return r;
+    return r;*/
 }
 
 return_type fsWrite(int nparams, arg_type* a){
     if (nparams != 3) {
 		r.return_val = NULL;
 		r.return_size = 0;
+		return r;
 	}
 
 	int fd = a->arg_val;
 	char *buffer = a->next->arg_val;
 	int count = a->next->next->arg_val;
+	int bytesWritten = -1;
 
-    if(!isLocked(fd)){
+    op_current = op_head;
+    while(op_current != NULL){
+        if(op_current->fd == fd && op_current->fd == fd){
+            bytesWritten = write(fd, buffer, count);
+            break;
+        }
+        op_current = op_current->next;
+    }
+    r.return_val = (void *)bytesWritten;
+    r.return_size = sizeof(int);
+    return r;
+    /*if(!isLocked(fd)){
         int bytesWritten = write(fd, buffer, count);
         if(bytesWritten > 0){
             r.return_val = (char *)bytesWritten;
@@ -331,10 +487,11 @@ return_type fsWrite(int nparams, arg_type* a){
         }
     }else{
         return error_val;
-    }
+    }*/
 }
 
 return_type fsRemove(int nparams, arg_type* a){
-
+    //loop through the code and find the file with the filename
+    //delete the file if it is not locked
 }
 
